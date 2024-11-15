c37822a3f3e07157a5c607ca1d73c5a02a04128f
D. PRAKASH <d.prakash2643@gmail.com>
2024-11-15 20:19:36 +0530

    Delete class directory

--- class/2_dataconnectin/acces.py
+++
@@ -1,6 +0,0 @@
-for index, row in df.iterrows():
-    company_name = row['Company Name']
-    location = row['Location']
-    email = row['Email']
-    products = row['Products']
-    print(f"Company: {company_name}, Location: {location}, Email: {email}, Products: {products}")
